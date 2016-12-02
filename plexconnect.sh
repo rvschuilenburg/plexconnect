f8c9754027c3662f703d0d10050c6b91ec921641
Robin van Schuilenburg <rvschuilenburg@gmail.com>
2016-12-02 10:50:16 +0100

    experimenting with symbolic links for config

--- plexconnect.sh
+++ plexconnect.sh
@@ -7,13 +7,15 @@
   else
     /usr/bin/python /opt/plexconnect/Settings.py 
     mv /opt/plexconnect/Settings.cfg /config/Settings.cfg
-    mv /opt/plexconnect/ATVSettings.cfg /config/ATVSettings.cfg
     ln -s /config/Settings.cfg /opt/plexconnect/Settings.cfg 
-    ln -s /config/ATVSettings.cfg /opt/plexconnect/ATVSettings.cfg
   fi
 
  echo "*** Check if previous ATVconfig exist, if so will use it..."  
   if [ -f /config/ATVSettings.cfg ]; then   
+    ln -s /config/ATVSettings.cfg /opt/plexconnect/ATVSettings.cfg
+  else
+    /usr/bin/python /opt/plexconnect/ATVSettings.py
+    mv /opt/plexconnect/ATVSettings.cfg /config/ATVSettings.cfg
     ln -s /config/ATVSettings.cfg /opt/plexconnect/ATVSettings.cfg
   fi
 
